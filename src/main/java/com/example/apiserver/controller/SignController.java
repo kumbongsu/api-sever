b48d5e4e6eddf92682eb158a68ae16f193bb5e77
kumbongsu <kbsgoldfish@nate.com>
2022-04-25 14:28:05 +0900

    first commit

---
+++ src/main/java/com/example/apiserver/controller/SignController.java
@@ -0,0 +1,59 @@
+package com.example.apiserver.controller;
+
+import com.example.apiserver.advice.exception.CEmailSigninFailedException;
+import com.example.apiserver.config.security.JwtTokenProvider;
+import com.example.apiserver.entity.User;
+import com.example.apiserver.model.response.CommonResult;
+import com.example.apiserver.model.response.SingleResult;
+import com.example.apiserver.repository.UserJpaRepository;
+import com.example.apiserver.service.ResponseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@Api(tags = {"1. Sign"})
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/v1")
+public class SignController {
+
+    private final UserJpaRepository userJpaRepo; // jpa 쿼리 활용
+    private final JwtTokenProvider jwtTokenProvider; // jwt 토큰 생성
+    private final ResponseService responseService; // API 요청 결과에 대한 code, messageㅍ
+    private final PasswordEncoder passwordEncoder; // 비밀번호 암호화
+
+    @ApiOperation(value = "로그인", notes = "이메일 회원 로그인을 한다.")
+    @PostMapping(value = "/signin")
+    public SingleResult<String> signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
+                                       @ApiParam(value = "비밀번호", required = true) @RequestParam String password) {
+        User user = userJpaRepo.findByUid(id).orElseThrow(CEmailSigninFailedException::new);
+        if (!passwordEncoder.matches(password, user.getPassword()))
+            // matches : 평문, 암호문 패스워드 비교 후 boolean 결과 return
+            throw new CEmailSigninFailedException();
+
+        return responseService.getSingleResult(jwtTokenProvider.createToken(String.valueOf(user.getMsrl()), user.getRoles()));
+    }
+
+    @ApiOperation(value = "가입", notes = "회원가입을 한다.")
+    @PostMapping(value = "/signup")
+    public CommonResult signup(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
+                               @ApiParam(value = "비밀번호", required = true) @RequestParam String password,
+                               @ApiParam(value = "이름", required = true) @RequestParam String name) {
+
+        userJpaRepo.save(User.builder()
+                .uid(id)
+                .password(passwordEncoder.encode(password))
+                .name(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+        return responseService.getSuccessResult();
+    }
+}
---
+++ src/main/java/com/example/apiserver/repository/UserJpaRepository.java
@@ -0,0 +1,11 @@
+package com.example.apiserver.repository;
+
+
+import com.example.apiserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserJpaRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUid(String email);
+}
